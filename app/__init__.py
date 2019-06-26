e7b2fb21bc25a2361bbfdd482f15cb4b777f58ee
unknown <carl.castello96@gmail.com>
2019-06-25 21:58:46 -0600

    Moved routes to init because it's the entry

--- app/__init__.py
+++ app/__init__.py
@@ -1,13 +1,23 @@
 from flask import Flask
+
 from config import config
+
+from app.extensions import DB
+
+
+def register_extensions(flask_app: Flask) -> None:
+    DB.init_app(flask_app)
 
 
 def create_app(config_name: str) -> Flask:
-    flask_app = Flask(__name__)
+    flask_app: Flask = Flask(__name__)
     flask_app.config.from_object(config[config_name])
     config[config_name].init_app(flask_app)
 
-    from app.main.routes import get_routes_blueprint
+    register_extensions(flask_app)
+
+    # Route Blue prints
+    from app.main import get_routes_blueprint
     flask_app.register_blueprint(get_routes_blueprint(), url_prefix='')
 
     return flask_app
